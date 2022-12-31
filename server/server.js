b9ef96813912bb78aa411661d25ab36696d47d6f
Abdul <Abdul@sandeza-inc.com>
2022-12-31 10:21:22 +0530

    text message

--- server/server.js
+++ server/server.js
@@ -1,10 +1,135 @@
-function searchUser(passData, domain) {
+function createConversation(user_id, message_content, iparams) {
+  let user_key = user_id
+  let content = message_content
+  console.log("createConversation iparams", iparams.freshChat_key);
+  const requestUrl = `${iparams.freshchat_domain}/conversations`
+  const requestOptions = {
+    headers: {
+      Authorization: 'Bearer <%= iparam.freshChat_key %>'
+    },
+    isOAuth: true,
+    json:
+    // {
+    //   "messages": [
+    //     {
+    //       "app_id": "4c5cdfa2-9070-4a62-abf2-7d25953b954e",
+    //       "actor_type": "user",
+    //       "actor_id": user_key,
+    //       "channel_id": "39829688-cf36-4424-a31b-e961e56b2dc4",
+    //       "message_type": "normal",
+    //       "message_parts": [
+    //         {
+    //           "text": {
+    //             "content": content
+    //           }
+    //         }
+    //       ]
+    //     }
+    //   ],
+    //   "status": "new",
+    //   "users": [
+    //     {
+    //       "id": user_key
+    //     }
+    //   ]
+    // },
+    {
+      app_id: "4c5cdfa2-9070-4a62-abf2-7d25953b954e",
+      channel_id: "39829688-cf36-4424-a31b-e961e56b2dc4",
+      messages: [
+        {
+          app_id: "4c5cdfa2-9070-4a62-abf2-7d25953b954e",
+          actor_type: "user",
+          actor_id: user_key,
+          channel_id: "39829688-cf36-4424-a31b-e961e56b2dc4",
+          message_type: "normal",
+          message_parts: [
+            {
+              text: {
+                content: content
+              }
+            }
+          ]
+        }
+      ],
+      status: "new",
+      users: [
+        {
+          id: user_key
+        }
+      ]
+    }
+  };
+  $request.post(requestUrl, requestOptions).then(function (data) {
+    console.log("this is whole data from createConversation===>", data);
+    let result = JSON.parse(data.response);
+    console.log("this is whole result from createConversation===>", result);
+
+    passData = {}
+    passData.user_key = user_key
+    passData.content = content
+
+    if (result.id == []) {
+      getConversationId(passData, domain, iparams)
+    }
+
+  }, function (err) {
+    console.log('error message from createConversation =====>', err);
+  });
+
+}
+
+function createUser(user_key, content, iparams) {
+  console.log("iparams", iparams);
+  console.log("iparams", iparams.freshChat_key);
+  console.log("iparams", user_key);
+  console.log("iparams", content);
+
+  // let user_key = user_key
+  // let content = content
+  const requestUrl = `${iparams.freshchat_domain}/users`
+
+  const requestOptions = {
+    headers: {
+      Authorization: 'Bearer <%= iparam.freshChat_key %>',
+      "Content-Type": "application/json"
+    },
+    isOAuth: true,
+    json: {
+      "reference_id": user_key
+    }
+  };
+  console.log("requestOptions", requestOptions);
+  console.log("requestUrl", requestUrl);
+  $request.post(requestUrl, requestOptions).then(function (data) {
+    console.log("this is whole data from createUser===>", data);
+    let result = data.response;
+    console.log("this is whole result from createUser===>", result);
+
+    passData = {}
+    passData.user_key = result.reference_id
+    passData.userId = result.id
+    passData.content = content
+
+    getUserConversationId(passData, iparams)
+
+  }, function (err) {
+    console.log('error message from createUser =====>', err);
+  });
+
+}
+
+
+
+
+
+function searchUser(passData, iparams) {
   let user_key = passData.user_key
   let content = passData.content
-  console.log("domain", domain);
+  console.log("iparams", iparams);
   console.log("content form seaarch =====>", content);
   console.log("referance id for searchUser", passData);
-  const requestUrl = `${domain}/users?reference_id=${user_key}`
+  const requestUrl = `${iparams.freshchat_domain}/users?reference_id=${user_key}`
   console.log("request url: for searchUser", requestUrl);
   const requestOptions = {
     headers: {
@@ -12,32 +137,42 @@ function searchUser(passData, domain) {
     },
   };
   $request.get(requestUrl, requestOptions).then(function (data) {
+    console.log("this is whole data from searchUser==1", data);
     let result = JSON.parse(data.response);
-    let userlist = (result.users.reverse());
-    let userArr = (userlist[0]);
-    console.log("userArr all user here =========> 1", userArr);
-    let userId = userArr.id
-    console.log("userId= 1", userArr.id);
+    console.log("this is whole result from searchUser==1", result);
 
-    let passData = {}
-    passData.userId = userId;
-    passData.content = content;
-    console.log('User Information =====> 1', userId);
-    getUserConversationId(passData, domain)
-  }, function (err) {
-    console.log('error message from search user =====>', err);
-  });
+    console.log('result.users =====> 1', result.users);
+    if (result.users.length === 0) {
+      createUser(user_key, content, iparams)
+    } else {
+      let userlist = (result.users.reverse());
+      let userArr = (userlist[0]);
+      console.log("userArr all user here =========> 1", userArr);
+      let userId = userArr.id
+      console.log("userId= 1", userArr.id);
+      let passData = {}
+      passData.userId = userId;
+      passData.content = content;
+
+      console.log('User Information =====> 1', userId);
+      getUserConversationId(passData, iparams)
+    }
+
+  }
+    , function (err) {
+      console.log('error message from search user =====>', err);
+    });
 
 }
 
-function getUserConversationId(value, domain) {
+function getUserConversationId(value, iparams) {
   console.log("value from getUserConversation 2", value);
   console.log("message content 2", value.content);
   let user_id = value.userId
   let message_content = value.content
 
   console.log("referance id for searchUser 2", user_id);
-  const requestUrl = `${domain}/users/${user_id}/conversations`
+  const requestUrl = `${iparams.freshchat_domain}/users/${user_id}/conversations`
   console.log("request url: for getUserConversation 2", requestUrl);
   const requestOptions = {
     headers: {
@@ -52,25 +187,27 @@ function getUserConversationId(value, domain) {
     let valueMap = conversationArr.map((item) => (item.id));
     let result = valueMap.toString();
     console.log('Conversation ID =====> 2', result);
-
-    let passData = {}
-    passData.userId = user_id
-    passData.content = message_content
-    passData.conversationId = result
-    console.log("passData: from getUserConversation 2", passData);
-    getConversationDetails(passData, domain)
-
+    if (conversationArr.length == 0) {
+      createConversation(user_id, message_content, iparams)
+    } else {
+      let passData = {}
+      passData.userId = user_id
+      passData.content = message_content
+      passData.conversationId = result
+      console.log("passData: from getUserConversation 2", passData);
+      getConversationDetails(passData, iparams)
+    }
   }, function (err) {
-    console.log('error message from search user =====>', err);
+    console.log('error message from getUserConversationId =====>', err);
   });
 }
 
-function getConversationDetails(value, domain) {
+function getConversationDetails(value, iparams) {
   console.log("conversation Id 3", value);
   var conversation_id = value.conversationId
   console.log("conversation_id 3", conversation_id);
   console.log("conversation_id 3", conversation_id);
-  const requestUrl = `${domain}/conversations/${conversation_id}`
+  const requestUrl = `${iparams.freshchat_domain}/conversations/${conversation_id}`
   console.log("request url: for getUser 3", requestUrl);
   const requestOptions = {
     headers: {
@@ -88,16 +225,16 @@ function getConversationDetails(value, domain) {
     passData.channel_id = result.channel_id
     passData.skill_id = result.skill_id
     passData.content = value.content
-    sendMessageToAgent(passData, domain)
+    sendMessageToAgent(passData, iparams)
   }, function (err) {
     console.log('error message from search user =====>', err);
   });
 }
 
-function sendMessageToAgent(payload, domain) {
+function sendMessageToAgent(payload, iparams) {
   console.log("payload from conversation: 4" + JSON.stringify(payload))
   let conversation_id = payload.conversation_id
-  const requestUrl = `${domain}/conversations/${conversation_id}/messages`
+  const requestUrl = `${iparams.freshchat_domain}/conversations/${conversation_id}/messages`
   var headers = {
     "Authorization": 'Bearer <%= iparam.freshChat_key %>',
     "Content-Type": "application/json"
@@ -125,7 +262,9 @@ function sendMessageToAgent(payload, domain) {
       console.log("sendMessageToAgent =====> 4", data.response);
     }
 
-  }).catch((err) => { console.log(err) })
+  }, function (err) {
+    console.log('error message from search user =====>', err);
+  })
 
 }
 
@@ -133,12 +272,12 @@ function sendMessageToAgent(payload, domain) {
 
 
 
-function searchUserForAgent(payload, domain) {
-  console.log("searchUserForAgent", payload, domain);
+function searchUserForAgent(payload, iparams) {
+  console.log("searchUserForAgent", payload, iparams);
   let userId = payload.user_id;
   let message_type = payload.message_type;
   let message_content = payload.content;
-  const requestUrl = `${domain}/users/${userId}`
+  const requestUrl = `${iparams.freshchat_domain}/users/${userId}`
   console.log("request url: for searchUser from agent message", requestUrl);
   const requestOptions = {
     headers: {
@@ -204,34 +343,33 @@ exports = {
   onAppInstallCallback: async function (payload) {
     console.log("onapp install", payload);
     let thirdPartyEndpoint = payload.iparams.webhook_url
-    // console.log("this is third party endpoint", thirdPartyEndpoint);
-    try {
-      const webhook = await generateTargetUrl();
-      const options = {
-        body: `{'webhook': ${webhook}}`,
-        action: 'register',
-        json: {
-          user_key: payload.user_key,
-          session_id: payload.session_id,
-          sender_key: payload.sender_key,
-          time: payload.time,
-          serial_number: payload.serial_number,
-          type: payload.type,
-          content: payload.content
-        }
-      };
-      console.log("------------", options);
-      const { response } = await $request.post(thirdPartyEndpoint, options);
-      renderData();
+    console.log("this is third party endpoint", thirdPartyEndpoint);
+    // try {
+    const webhook = await generateTargetUrl();
+    // const options = {
+    //   // body: `{'webhook': ${webhook}}`,
+    //   // action: 'register',
+    //   // json: {
+    //   //   //   user_key: payload.data.user_key,
+    //   //   //   session_id: payload.data.session_id,
+    //   //   //   sender_key: payload.data.sender_key,
+    //   //   //   time: payload.data.time,
+    //   //   //   serial_number: payload.data.serial_number,
+    //   //   //   type: payload.data.type,
+    //   //   //   content: payload.data.content
+    //   // }
+    // };
+    // const { response } = await $request.post(options);
 
-      console.info('\n Webhook creation successful \n', webhook);
-      console.info('\n Webhook Registration Successful \n', response);
-      console.info('\n Hander received following payload when app is installed \n\n', payload);
-
-    } catch (error) {
-      console.error('Something went wrong. Webhook Registration has failed', error);
-    }
+    console.info('\n Webhook creation successful \n', webhook);
+    // console.info('\n Webhook Registration Successful \n', response);
+    console.info('\n Hander received following payload when app is installed \n\n', payload);
     renderData();
+
+    // } catch (error) {
+    //   console.error('Something went wrong. Webhook Registration has failed', error);
+    // }
+    // renderData();
   },
   /**
   * Handler for onAppUninstall event
@@ -244,7 +382,7 @@ exports = {
   onAppUninstallCallback: async function (payload) {
     console.log("uninstall", payload);
     let thirdPartyEndpoint = payload.iparams.webhook_url
-    // console.log("this is third party endpoint", thirdPartyEndpoint);
+    console.log("this is third party endpoint", thirdPartyEndpoint);
     try {
       const options = {
         action: 'de-register'
@@ -259,24 +397,27 @@ exports = {
   },
 
   onExternalEventCallback: function (payload) {
-    var domain = payload.iparams.freshchat_domain;
+    // var domain = payload.iparams.freshchat_domain;
+    var iparams = payload.iparams
+    console.log("iparams: from on external event ", iparams);
     // console.log("========this` is external freshchat url=======", domain);
     console.log("onExternalEventCallback payload ====>", (payload));
     const { data } = (payload);
     // let externalData = (data);
     if (typeof (data) == "string") {
-      externalData = (data)
+      externalData = JSON.parse(data)
     } else {
       externalData = (data)
     }
     let passData = {}
     passData.user_key = externalData.user_key
+    passData.session_id = externalData.session_id
     passData.sender_key = externalData.sender_key
     passData.serial_number = externalData.serial_number
     passData.type = externalData.type
     passData.content = externalData.content
-    console.log("passdata: ", passData);
-    searchUser(passData, domain);
+    console.log("passdata: external event ", passData);
+    searchUser(passData, iparams);
 
   },
   onUserCreateCallback: async function (payload) {
@@ -365,26 +506,74 @@ exports = {
     });
   },
   onMessageCreateCallback: async function (payload) {
-    var domain = payload.iparams.freshchat_domain;
-    // console.log("========this is onMessageCreateCallback freshchat url=======", domain);
-    console.log("========this is onMessageCreateCallback payload=======", payload);
-    console.log("========this is onMessageCreateCallback payload=======", payload.event);
-    let messageData = payload.data.conversation.message_parts
-    let messageArr = messageData.map((item) => (item.text.content));
+
+
+    console.log("onMessageCreateCallback payload", payload);
+    console.log("onMessageCreateCallback iparams", payload.iparams);
+    let iparams = payload.iparams
+
+    console.log("========this is onMessageCreateCallback payload=======", payload.data.message);
+    console.log("========this is onMessageCreateCallback messages=======", payload.data.message.messages);
+
+    let messageData = payload.data.message.messages
+
+
+    let messageArr = messageData.map((item) => (item));
     console.log("messageArr", messageArr);
-    console.log("messageArr", messageData);
-    let message = messageArr.toString();
+
+    let messageArrparts = messageData.map((item) => (item.message_parts));
+    console.log("messageArrparts", messageArrparts);
+
+
+
+    // 1
+    let messageArrUserId = messageData.map((item) => (item.user_id));
+    console.log("messageArrUserId", messageArrUserId.toString());
+    let user_id = messageArrUserId.toString()
+    // 2
+    let messageArrConversationId = messageData.map((item) => (item.conversation_id));
+    console.log("messageArrConversationId", messageArrConversationId.toString());
+    let conversation_id = messageArrConversationId.toString()
+    // 3
+    let messageArrActorType = messageData.map((item) => (item.actor_type));
+    console.log("messageArrActorType", messageArrActorType.toString());
+    let actor_type = messageArrActorType.toString()
+    // 4
+    let messageArrMessageType = messageData.map((item) => (item.message_type));
+    console.log("messageArrMessageType", messageArrMessageType.toString());
+    let message_type = messageArrMessageType.toString()
+    // 5
+    let messageArrContent = messageData.map((item) => (item.full_message_text));
+    console.log("messageArrContent", messageArrContent.toString());
+    let messageContent = messageArrContent.toString()
+
+
+
+
+
+
+
+
+
+
+
+    // let messageParts = messageData.map((item) => (item.message_parts));
+    // console.log("messageParts", messageParts);
+
+    // let message = messageArr.toString();
     let passData = {}
-    passData.user_id = payload.data.conversation.user_id
-    passData.conversation_id = payload.data.conversation.conversation_id
-    passData.actor_type = payload.data.conversation.actor_type
-    passData.message_type = payload.data.conversation.message_type
-    passData.content = message
+    // 1
+    passData.user_id = user_id
+    passData.conversation_id = conversation_id
+    passData.actor_type = actor_type
+    passData.message_type = message_type
+    passData.content = messageContent
     passData.client_id = payload.iparams.Kakao_Client_ID;
     passData.client_secret = payload.iparams.Kakao_Client_Secret;
     passData.sender_key = payload.iparams.Kakao_Sender_Key
-    searchUserForAgent(passData, domain)
-
+    if (actor_type == "agent") {
+      searchUserForAgent(passData, iparams)
+    }
 
   }
 };
